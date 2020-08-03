aabf4c99dbefd03c94446e318d9d775a60a55635
Mahendra Ladhe <mahendra.ladhe@broadcom.com>
2020-08-03 21:41:06 +0530

    Added KBP SDK 1.5.13 for two different platforms

---
+++ sdk-1.5.13/T1040_PPC_BE_32B/include/kbp_externcstart.h
@@ -0,0 +1,18 @@
+/*
+ * $Id$
+ * 
+ * This license is set out in https://raw.githubusercontent.com/Broadcom/Broadcom-Compute-Connectivity-Software-KBP-SDK/master/Legal/LICENSE file.
+ *
+ * $Copyright: (c) 2020 Broadcom Inc.
+ * All Rights Reserved.$
+ *
+ */
+
+/*
+ * We put this here so that auto-indenters do not get confused
+ * and force the top-level definitions to get indented
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
