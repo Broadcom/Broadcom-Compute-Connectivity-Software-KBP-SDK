8483c6289c88c3b76cd9bd08e1dc1a1b2d7c787e
Raghavendra Jadhav <rj939501@ibnvdb0689.ibn.broadcom.net>
2021-01-06 15:50:53 +0530

    KBP SDK libraries for version 1.4.22.2

---
+++ sdk-1.4.22/ARAD_WRLINUX_32B/include/default_allocator.h
@@ -0,0 +1,89 @@
+/*
+ * $Id$
+ * 
+ * This license is set out in https://raw.githubusercontent.com/Broadcom/Broadcom-Compute-Connectivity-Software-KBP-SDK/master/Legal/LICENSE file.
+ *
+ * $Copyright: (c) 2020 Broadcom Inc.
+ * All Rights Reserved.$
+ *
+ */
+
+#ifndef __DEFAULT_ALLOCATOR_H
+#define __DEFAULT_ALLOCATOR_H
+
+#include <stdint.h>
+
+#include "allocator.h"
+#include "errors.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @file default_allocator.h
+ *
+ * Internal implementation of allocator abstractions with bookkeeping.
+ * Additional memory is consumed per allocation for bookkeeping.
+ * Relies on standard malloc/free.
+ *
+ * @addtogroup DEFAULT_ALLOCATOR_API
+ * @{
+ */
+
+/**
+ * Creates a new default allocator that
+ * uses operations from stdlib.h and keeps
+ * statistics.
+ *
+ * @param alloc Allocator, initialized and returned on success.
+ *
+ * @return KBP_OK on success or an error code otherwise.
+ */
+
+ kbp_status default_allocator_create(struct kbp_allocator **alloc);
+
+/**
+ * Destroys the default allocator. If incorrect
+ * number of free calls are made, the memory may
+ * leak.
+ *
+ * @param alloc Valid allocator handle.
+ *
+ * @return KBP_OK on success or an error code otherwise.
+ */
+
+kbp_status default_allocator_destroy(struct kbp_allocator *alloc);
+
+/**
+ * Allocator statistics, bytes allocated, and
+ * number of allocation calls made.
+ */
+
+struct default_allocator_stats {
+    uint64_t peak_bytes;        /**< Maximum active bytes ever allocated. */
+    uint64_t cumulative_bytes;  /**< Total number of bytes allocated. */
+    uint32_t nallocs;           /**< Number of malloc/calloc calls. */
+    uint32_t nfrees;            /**< Number of free calls. */
+};
+
+/**
+ * Returns statistics associated with the allocator. These statistics are
+ * held internally and are cleared when read.
+ *
+ * @param alloc Valid allocator handle.
+ * @param stats Valid pointer to memory to be populated with statistics.
+ *
+ * @return KBP_OK on success or an error code otherwise.
+ */
+
+kbp_status default_allocator_get_stats(struct kbp_allocator *alloc, struct default_allocator_stats *stats);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+#endif                          /* __DEFAULT_ALLOCATOR_H */
